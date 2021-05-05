7eb9596e0383a53a099752200bb579aaef245632
vignesh <vickyrobotic7@outlook.com>
2021-05-05 17:01:24 +0530

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,22 @@ export class AppComponent {
     "Telungana"
   ];
 
+  relation = [
+    "Father",
+    "Mother",
+    "Brother",
+    "Sister",
+    "Wife",
+    "Husband",
+    "Son",
+    "Daughter",
+    "GrandMother",
+    "GrandFather",
+    "GrandSon",
+    "GrandDaughter"
+  ];
+  addresscard;
+
   submitted = false;
 
   userForm: FormGroup;
@@ -49,6 +65,14 @@ export class AppComponent {
       ]),
       family: new FormArray([])
     });
+
+    if(this.userForm.get('family').value){
+      console.log(this.userForm.get('family').value)
+    }
+    let familyarray = this.userForm.get('family').value
+    this.addresscard = familyarray.length;
+
+    console.log(this.userForm.get('family').value)
   }
 
   get f() {
@@ -65,6 +89,29 @@ export class AppComponent {
     });
   }
 
+  familydetail(){
+    return this.fb.group({
+      relationship:new FormControl("", Validators.required),
+      rel_firstname: new FormControl("", Validators.required),
+      rel_lastname: new FormControl("", Validators.required),
+      rel_email: new FormControl("", [Validators.required, Validators.email]),
+      rel_phone: new FormControl(null, [
+        Validators.required,
+        Validators.min(1000000000),
+        Validators.max(9999999999)
+      ]),
+      rel_gender: new FormControl("", Validators.required),
+      rel_addressLine: new FormArray([this.addressline()]),
+      rel_city: new FormControl("", Validators.required),
+      rel_state: new FormControl("", Validators.required),
+      rel_pincode: new FormControl(null, [
+        Validators.required,
+        Validators.min(100000),
+        Validators.max(999999)
+      ])
+    });
+  }
+
   addAddressLine() {
     let addressLine = this.userForm.get("addressLine") as FormArray;
     addressLine.push(this.addressline());
@@ -72,7 +119,31 @@ export class AppComponent {
 
   removeLine(i) {
     let addressLine = this.userForm.get("addressLine") as FormArray;
+    addressLine.removeAt(i);
+  }
 
+  addFamily(){
+    let relation = this.userForm.get('family') as FormArray;
+    relation.push(this.familydetail());
+    this.addresscard = relation.length;
+  }
+
+  removeFamily(j){
+    let relation = this.userForm.get('family') as FormArray;
+    relation.removeAt(j);
+    this.addresscard = relation.length;
+  }
+
+  addfamilyLine(j){
+    let family = this.userForm.get('family') as FormArray;
+    let addressLine = family.controls[j].get('rel_addressLine') as FormArray;
+    // console.log(family.controls[0].get('addressLine').value)
+     addressLine.push(this.addressline());
+  }
+
+  removefamilyLine(j,i){
+    let family = this.userForm.get('family') as FormArray;
+    let addressLine = family.controls[j].get('rel_addressLine') as FormArray;
     addressLine.removeAt(i);
   }
 
